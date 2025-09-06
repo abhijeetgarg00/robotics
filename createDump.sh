b33a4370f63ff837dcf1012e300917c2c9d3d942
abhijeetgarg00 <abhijeetgarg007@gmail.com>
2025-09-06 13:17:39 -0400

    complete work env

--- createDump.sh
+++ createDump.sh
@@ -6,6 +6,3 @@ find /home/beast/ros2_ws/src/moveit_resources/panda_moveit_config \
     cat "$file"
     echo    # blank line separator
 done > /home/beast/ros2_ws/_chatgpt/arm_full_dump.txt
-
-cd ~/ros2_ws && colcon build --packages-select moveit_resources_panda_moveit_config && source install/setup.bash
-ros2 run moveit_resources_panda_moveit_config panda_move_example
